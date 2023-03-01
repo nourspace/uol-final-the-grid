74d3cec6636f43dad3fd0fa1d310fd1bcd430b4b
N Space <1257310+nourspace@users.noreply.github.com>
2023-03-01 15:37:33 +0100

    feat: initialise Nuxt project with TS

---
+++ frontend/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ nour }}
+    <!--    <NuxtWelcome title="nono333" app-name="jeeeeu" />-->
+    <!--    <my-header />-->
+  </div>
+  <div a="1" b="2" c="3" d="4"></div>
+</template>
+
+<script setup lang="ts">
+import MyHeader from '~/components/MyHeader.vue'
+
+let nour = 1
+const aa = '22222'
+console.log(aa + nour)
+</script>
---
+++ frontend/components/MyHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>{{ mo }}</div>
+</template>
+
+<script setup lang="ts">
+const mo = 'no'
+</script>
