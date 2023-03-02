e7244b1aebd49447d0dbaf29cf68087dfd4c268f
N Space <1257310+nourspace@users.noreply.github.com>
2023-03-02 15:14:56 +0100

    feat: Vue store example with Pinia

--- grid/src/stores/counter.ts
+++ grid/src/stores/counter.ts
@@ -4,8 +4,9 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+
+  function increment(howMuch: number = 1) {
+    count.value += howMuch
   }
 
   return { count, doubleCount, increment }
--- grid/src/views/AboutView.vue
+++ grid/src/views/AboutView.vue
@@ -1,15 +1,29 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore()
+const { count, doubleCount } = storeToRefs(store)
+const { increment } = store
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click.prevent="increment()">Click for 1</button>
+    <button @click.prevent="increment(2)">Click for 2</button>
+    <p>counter: {{ count }}</p>
+    <p>double: {{ doubleCount }}</p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 50vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: space-evenly;
   }
 }
 </style>
