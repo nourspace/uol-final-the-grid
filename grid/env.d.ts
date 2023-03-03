5a5da6d610e5a312d9296803d51681a4a11408c1
N Space <1257310+nourspace@users.noreply.github.com>
2023-03-03 22:42:57 +0100

    feat: integrate GraphQL; ApolloClient, graphql-tag, @vue/apollo-composable

--- grid/env.d.ts
+++ grid/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+// Todo: type Any allows named imports while the DocumentNode doesn't
+// Check how to export types
+// https://the-guild.dev/graphql/codegen
+// https://www.apollographql.com/docs/react/development-testing/static-typing/
+
+declare module '*.gql';
+// declare module '*.gql' {
+//   import { DocumentNode } from 'graphql'
+//   const Schema: DocumentNode
+//   export = Schema
+// }
+
+declare module '*.graphql';
+// declare module '*.graphql' {
+//   import { DocumentNode } from 'graphql'
+//   const Schema: DocumentNode
+//   export = Schema
+// }
--- grid/vite.config.ts
+++ grid/vite.config.ts
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import graphql from "@rollup/plugin-graphql"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), graphql()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
