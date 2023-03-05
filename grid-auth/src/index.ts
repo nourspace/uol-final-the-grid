ba6f238bfc8a17c873e4982099a47134205b10dc
N Space <1257310+nourspace@users.noreply.github.com>
2023-03-05 03:33:29 +0100

    feat: improve auth server; validation, error handling, ts config

--- grid-auth/src/index.ts
+++ grid-auth/src/index.ts
@@ -1,75 +1,101 @@
-import express, { Request, Response } from "express";
+import "express-async-errors"; // Important to import on top
+import express, { Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import { gql } from "graphql-request";
 import { client } from "./client";
 import { generateJWT } from "./jwt";
+import { body, validationResult } from "express-validator";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || "3000";
 
 // Parse JSON in request bodies
 app.use(express.json());
 
-app.post("/auth/register", async (req: Request, res: Response) => {
-  const { email, password } = req.body as Record<string, string>;
-
-  // In production app, you would check if user is already registered
-  // We skip that in this tutorial for the sake of time
-
-  // We insert the user using a mutation
-  // Note that we salt and hash the password using bcrypt
-  const { insert_user_one } = await client.request(
-    gql`
-      mutation registerUser($user: user_insert_input!) {
-        insert_user_one(object: $user) {
-          id
-        }
-      }
-    `,
-    {
-      user: {
-        email,
-        password: await bcrypt.hash(password, 10),
-      },
+app.post(
+  "/auth/register",
+  // username must be an email
+  body("username").not().isEmpty().isString().trim().escape().isString(),
+  // password must be at least 5 chars long
+  body("password")
+    .not()
+    .isEmpty()
+    .isString()
+    .trim()
+    .escape()
+    .isLength({ min: 5 }),
+  async (req: Request, res: Response) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
 
-  const { id: userId } = insert_user_one;
+    const { username, password } = req.body as Record<string, string>;
 
-  res.send({
-    token: generateJWT({
-      defaultRole: "user",
-      allowedRoles: ["user"],
-      otherClaims: {
-        "X-Hasura-User-Id": userId,
-      },
-    }),
-  });
-});
+    // In production app, you would check if user is already registered
+    // We skip that in this tutorial for the sake of time
+
+    // We insert the user using a mutation
+    // Note that we salt and hash the password using bcrypt
+    const { insert_users_one } = await client.request(
+      gql`
+        mutation registerUser($user: users_insert_input!) {
+          insert_users_one(object: $user) {
+            id
+          }
+        }
+      `,
+      {
+        user: {
+          username,
+          password: await bcrypt.hash(password, 10),
+        },
+      }
+    );
+
+    const { id: userId } = insert_users_one;
+
+    res.send({
+      token: generateJWT({
+        defaultRole: "user",
+        allowedRoles: ["user"],
+        otherClaims: {
+          "X-Hasura-User-Id": userId,
+        },
+      }),
+    });
+  }
+);
 
 app.post("/auth/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body as Record<string, string>;
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-  let { user } = await client.request(
+  const { username, password } = req.body as Record<string, string>;
+
+  const { users } = await client.request(
     gql`
-      query getUserByEmail($email: String!) {
-        user(where: { email: { _eq: $email } }) {
+      query getUserByUsername($username: String!) {
+        users(where: { username: { _eq: $username } }) {
           id
           password
         }
       }
     `,
     {
-      email,
+      username,
     }
   );
 
   // Since we filtered on a non-primary key we got an array back
-  user = user[0];
+  const user = users[0];
 
   if (!user) {
-    res.sendStatus(401);
-    return;
+    return res.status(401).json({ error: "Invalid username or password" });
   }
 
   // Check if password matches the hashed version
@@ -90,6 +116,29 @@ app.post("/auth/login", async (req: Request, res: Response) => {
   }
 });
 
+// Error handling
+app.use(logErrors);
+app.use(clientErrorHandler);
+
 app.listen(port, () => {
   console.log(`Auth server running on port ${port}.`);
 });
+
+function logErrors(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.status(500).send({ error: "Something failed!" });
+}
