7514a0e7a09d60690b78fd44d3ce493e35a69326
N Space <1257310+nourspace@users.noreply.github.com>
2023-03-27 19:10:57 +0200

    feat(ui): add tests for home and auth views

--- grid-ui/src/App.spec.ts
+++ grid-ui/src/App.spec.ts
@@ -1,17 +1,18 @@
 import App from '@/App.vue'
 import vuetify from '@/plugins/vuetify'
 import router from '@/router'
+// @ts-ignore not sure why pinia/testing has no type defs
+import { createTestingPinia } from "@pinia/testing"
 import { render } from '@testing-library/vue'
-import { createPinia } from 'pinia'
 import { describe, expect, it } from 'vitest'
 
-const mountOptions = { global: { plugins: [createPinia(), router, vuetify] }, props: {} }
+const mountOptions = { global: { plugins: [createTestingPinia(), router, vuetify] }, props: {} }
 
 describe('App', () => {
   it('renders properly', () => {
     render(App, mountOptions)
   })
-  it('has correct items', () => {
+  it('has correct tabs and login button', () => {
     const { getByRole } = render(App, mountOptions)
     const tablist = getByRole('tablist')
     expect(tablist.children.length).toBe(1)
